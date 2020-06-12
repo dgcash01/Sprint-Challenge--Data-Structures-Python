6c0dfa4c85a9ee206370fa23112a0884b0d2fcf4
Dan Cashion <dangcashion@gmail.com>
2020-06-12 12:18:03 -0500

    MVP and Stretch

--- names/names.py
+++ names/names.py
@@ -1,5 +1,35 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def dupes(self, target):
+        if target == self.value:
+            return duplicates.append(target)
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.dupes(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.dupes(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,6 +43,9 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+
+
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
@@ -22,7 +55,19 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+start_time = time.time()
+
+# Faster and preserves original order
+seen = {}
+new_list = [seen.setdefault(x, x) for x in name_1 if x not in seen]
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
